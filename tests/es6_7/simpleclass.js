f14342080ca79e6912028974bef2ec13480b7125
Dekel Cohen <dekelcohen@gmail.com>
2016-04-30 16:23:29 +0300

    ES6/7 Tests class and class body syntax: simpleclass.js

---
+++ tests/es6_7/simpleclass.js
@@ -0,0 +1,28 @@
+class MyClass {
+
+  method1() 
+  {  
+
+  }
+
+  arrowMethodProp1 = () => 
+  { 
+
+  }
+
+  methodProp = function (a,b) 
+  { 
+
+  }
+
+  static staticMethod() {
+            return 'classy';
+  }
+
+  static defaultProps = {
+    connected: false,
+    loading: false,
+    autoLoad: false
+  }
+  
+}
